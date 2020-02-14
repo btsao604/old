492a7b2de46bcebf6b434ef092fa7f546d4c43b1
btsao604 <Boris.Tsao@Boriss-MacBook-Pro.local>
2020-02-14 14:10:53 -0800

    committing second change

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hi there</div>;
+  return <div className="App">Testing changes</div>;
 }
 
 export default App;
